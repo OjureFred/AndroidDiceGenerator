ada38922b6bed90853aa704d30fbfc4845bdfa15
Fredrick Ojure <fredojure@hotmail.com>
2021-10-28 19:10:35 +0300

    Added button to layout and dice function

--- app/src/main/java/com/example/kotlindicegenerator/MainActivity.kt
+++ app/src/main/java/com/example/kotlindicegenerator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kotlindicegenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun dice(view: View) {
+        val counterArr = arrayOf(-1,0,0,0,0,0,0)
+        val newDice:Int = Random.nextInt(0, 7)
+        
+        println("Hello")
     }
 }
