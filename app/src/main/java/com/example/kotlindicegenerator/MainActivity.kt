cdf4d6d079a60be3c4299ccef09597f504330cc7
Fredrick Ojure <fredojure@hotmail.com>
2021-10-28 19:18:03 +0300

    Final solution

--- app/src/main/java/com/example/kotlindicegenerator/MainActivity.kt
+++ app/src/main/java/com/example/kotlindicegenerator/MainActivity.kt
@@ -14,7 +14,12 @@ class MainActivity : AppCompatActivity() {
     fun dice(view: View) {
         val counterArr = arrayOf(-1,0,0,0,0,0,0)
         val newDice:Int = Random.nextInt(0, 7)
-        
-        println("Hello")
+
+        for(i:Int in 1..1_000_000){
+            counterArr[newDice]++
+        }
+
+       for(i:Int in 1..counterArr.size-1)
+           println("My Dice | $i is ${counterArr[i]}")
     }
 }
